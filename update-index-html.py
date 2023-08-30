de635d5188937639646fd54638a40293fb46e1f2
U-CPHDOMAIN\dtr <dtr@siteimprove.com>
2023-08-29 21:00:41 -0500

    Adding a program that generates index.html.  b/c I want the crawler to index all of these pages.

---
+++ update-index-html.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+os.chdir(os.path.dirname(sys.argv[0]))
+filename_of_this_program = os.path.basename(sys.argv[0])
+output_index_filename = 'index.html'
+excluded_file_relative_paths = ['./'+output_index_filename, './'+filename_of_this_program]
+excluded_dirnames = ['.git']
+
+python_script_name = os.path.basename(__file__)
+
+html_file_paths = []
+
+for root, dirs, files in os.walk('.'):
+	dirs[:] = [d for d in dirs if d not in excluded_dirnames]
+
+	for file in files:
+		if file.endswith('.html'):
+			file_path = os.path.join(root, file)
+			if file_path not in excluded_file_relative_paths:
+				html_file_paths.append(file_path)
+
+html_file_paths.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+
+with open(output_index_filename, "w") as index_file:
+	index_file.write("<!DOCTYPE html>\n")
+	index_file.write("<html>\n")
+	index_file.write("<head>\n")
+	index_file.write("  <title>Index</title>\n")
+	index_file.write("</head>\n")
+	index_file.write("<body>\n")
+	index_file.write("  <h1>Index</h1>\n")
+	index_file.write("  <ul>\n")
+
+	for html_file_path in html_file_paths:
+		index_file.write(f'	<li><a href="{html_file_path }">{html_file_path}</a></li>\n')
+
+	index_file.write("  </ul>\n")
+	index_file.write("</body>\n")
+	index_file.write("</html>\n")
+
+
