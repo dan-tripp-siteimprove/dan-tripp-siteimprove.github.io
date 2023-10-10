2d9fa1a2797b743549c1c5c69a99dea26b78e1b5
U-CPHDOMAIN\dtr <dtr@siteimprove.com>
2023-10-10 12:46:27 -0400

    .

--- update-index-html.py
+++ update-index-html.py
@@ -2,11 +2,13 @@
 
 import sys, os
 
+excluded_dirnames = ['.git']
+included_file_extensions = ['.html', '.xml']
+
 os.chdir(os.path.dirname(sys.argv[0]))
 filename_of_this_program = os.path.basename(sys.argv[0])
 output_index_filename = 'index.html'
 excluded_file_relative_paths = ['./'+output_index_filename, './'+filename_of_this_program]
-excluded_dirnames = ['.git']
 
 python_script_name = os.path.basename(__file__)
 
@@ -16,7 +18,7 @@ for root, dirs, files in os.walk('.'):
 	dirs[:] = [d for d in dirs if d not in excluded_dirnames]
 
 	for file in files:
-		if file.endswith('.html') or file.endswith('.xml'):
+		if any(file.lower().endswith(ext.lower()) for ext in included_file_extensions):
 			file_path = os.path.join(root, file)
 			if file_path not in excluded_file_relative_paths:
 				html_file_paths.append(file_path)
