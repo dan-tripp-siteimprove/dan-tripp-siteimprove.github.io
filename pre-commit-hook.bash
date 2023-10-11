3e62c575d72b31817740a7bc2ae7d1935f687e1a
U-CPHDOMAIN\dtr <dtr@siteimprove.com>
2023-10-11 16:37:19 -0400

    .

---
+++ pre-commit-hook.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+IFS=$'\n\t'  # Inspired by http://redsymbol.net/articles/unofficial-bash-strict-mode/.  Meant as a safety net.  You should still quote variable expansions.
+function err_trap_func () {
+	exit_status="$?"
+  echo "Exiting with status \"$exit_status\" due to command \"$BASH_COMMAND\" (call stack: line(s) $LINENO ${BASH_LINENO[*]} in $0)"
+	exit "$exit_status"
+}
+trap err_trap_func ERR
+
+function exit_trap_func () {
+	true
+}
+trap exit_trap_func EXIT
+
+set -o errtrace
+
+#cd "$(dirname "$0")"
+#set -o xtrace
+
+
+echo 'change cwd' # tdr 
+sleep 2 # tdr 
+
