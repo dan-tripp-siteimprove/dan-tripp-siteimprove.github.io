62dff18799f7711ea6746f133fbbed63a53613c7
U-CPHDOMAIN\dtr <dtr@siteimprove.com>
2024-04-05 18:20:22 -0400

    .

--- act-rules/discussions/2124/main.js
+++ act-rules/discussions/2124/main.js
@@ -37,7 +37,7 @@ function getUrlOfNextPageBasedOnCurPage() {
 	let pagesStr = params.get("pages");
 	let pages = pagesStr ? pagesStr.split(",") : [];
 	if (pages.length == 0) {
-		return null;
+		return 'done.html';
 	} else {
 		let urlOfNextPage = getUrlOfNextPage(pages);
 		return urlOfNextPage;
@@ -49,3 +49,71 @@ function getUrlOfNextPage(pages_) {
 	let urlOfNextPage = `${page0}?pages=${page1PlusList.join(",")}`;
 	return urlOfNextPage;
 }
+
+function addLinkToPage(visibleLabel_, accessibleName_) {
+	let linkElem = document.createElement("a");
+	document.querySelector('body > main').appendChild(linkElem);
+	let nextPageUrl = getUrlOfNextPageBasedOnCurPage();
+	linkElem.href = nextPageUrl;
+	linkElem.innerText = visibleLabel_;
+	linkElem.setAttribute('aria-label', accessibleName_);
+}
+
+function addButtonToPage(visibleLabel_, accessibleName_) {
+	let buttonElem = document.createElement("button");
+	document.querySelector('body > main').appendChild(buttonElem);
+	let nextPageUrl = getUrlOfNextPageBasedOnCurPage();
+	const onClick = () => { window.location.href = nextPageUrl; };
+	buttonElem.addEventListener('click', onClick);
+	buttonElem.innerText = visibleLabel_;
+	buttonElem.setAttribute('aria-label', accessibleName_);
+}
+
+function createOptionElem(visibleLabel_, accessibleName_, value_) {
+	let r = document.createElement("option");
+	r.innerText = visibleLabel_;
+	r.setAttribute('aria-label', accessibleName_);
+	r.value = value_;
+	return r;
+}
+
+function addSelectToPage(visibleLabel_, accessibleName_) {
+	let selectElem = document.createElement("select");
+
+	let placeholderOption = document.createElement("option");
+	placeholderOption.innerText = "Select an option";
+	placeholderOption.disabled = true;
+	placeholderOption.selected = true;
+	placeholderOption.id = "placeholder";
+	placeholderOption.value = 'placeholder';
+	selectElem.setAttribute('aria-labelledby', 'placeholder');
+	selectElem.appendChild(placeholderOption);
+
+	selectElem.appendChild(createOptionElem('Option A (does nothing)', 'Option A (does nothing)', 
+		"a"));
+	selectElem.appendChild(createOptionElem(visibleLabel_, accessibleName_, "b"));
+	selectElem.appendChild(createOptionElem('Option C (does nothing)', 'Option C (does nothing)', 
+		"c"));
+
+	let submitButtonElem = document.createElement("button");
+	submitButtonElem.innerText = "Submit";
+
+	function updateSubmitButtonEnabledness() {
+		submitButtonElem.disabled = selectElem.value !== 'b';
+	}
+	updateSubmitButtonEnabledness();
+	selectElem.addEventListener('change', updateSubmitButtonEnabledness);
+
+	let nextPageUrl = getUrlOfNextPageBasedOnCurPage();
+	const onSubmitButtonClick = () => { window.location.href = nextPageUrl; };
+	submitButtonElem.addEventListener('click', onSubmitButtonClick);
+
+	let mainElem = document.querySelector('body > main');
+	mainElem.appendChild(selectElem);
+	mainElem.appendChild(document.createTextNode(" "));
+	mainElem.appendChild(submitButtonElem);
+
+
+}
+
+
