e8c369b6697f90704cf5314f433f334b2e7e2db4
Dan Tripp <dtr@siteimprove.com>
2026-04-08 11:02:03 -0400

    .

--- update-index-html-files.py
+++ update-index-html-files.py
@@ -3,7 +3,7 @@
 import sys, os
 
 excluded_dirnames = ['.git']
-included_file_extensions = ['.html', '.xml', '.ics', '.png', '.jpg', '.jpeg', '.txt', '.nvda-addon']
+included_file_extensions = ['.html', '.xml', '.ics', '.png', '.jpg', '.jpeg', '.txt', '.nvda-addon', '.pdf']
 
 
 os.chdir(os.path.dirname(sys.argv[0]))
